8cc420fbf6871e738031c60732dfcd2309b3d70f
Ben Spark <bhrugen1990@gmail.com>
2020-07-10 15:15:44 -0500

    Section 2 - Create DbContext

---
+++ DapperDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DapperDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+}
