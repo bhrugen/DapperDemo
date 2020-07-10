fd6a36e48a0f747c5bdaabf25771e340bacc853b
Ben Spark <bhrugen1990@gmail.com>
2020-07-10 16:06:56 -0500

    Section 2 - Implement Entity Framework Repository

---
+++ DapperDemo/Repository/ICompanyRepository.cs
@@ -0,0 +1,20 @@
+﻿using DapperDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Repository
+{
+    public interface ICompanyRepository
+    {
+        Company Find(int id);
+        List<Company> GetAll();
+
+        Company Add(Company company);
+        Company Update(Company company);
+
+        void Remove(int id);
+
+    }
+}
