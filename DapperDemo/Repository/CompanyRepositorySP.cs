51e8279b344f5dfa2589fef25f4eb2c37a5142f5
Ben Spark <bhrugen1990@gmail.com>
2020-07-10 18:56:09 -0500

    Section 5 - Stored Procedure Update and Remove

--- DapperDemo/Repository/CompanyRepositorySP.cs
+++ DapperDemo/Repository/CompanyRepositorySP.cs
@@ -48,15 +48,20 @@ namespace DapperDemo.Repository
 
         public void Remove(int id)
         {
-            var sql = "DELETE FROM Companies WHERE CompanyId = @Id";
-            db.Execute(sql, new { id });
+            db.Execute("usp_RemoveCompany", new { CompanyId = id }, commandType: CommandType.StoredProcedure);
         }
 
         public Company Update(Company company)
         {
-            var sql = "UPDATE Companies SET Name = @Name, Address = @Address, City = @City, " +
-                "State = @State, PostalCode = @PostalCode WHERE CompanyId = @CompanyId";
-            db.Execute(sql, company);
+            var parameters = new DynamicParameters();
+            parameters.Add("@CompanyId", company.CompanyId, DbType.Int32);
+            parameters.Add("@Name", company.Name);
+            parameters.Add("@Address", company.Address);
+            parameters.Add("@City", company.City);
+            parameters.Add("@State", company.State);
+            parameters.Add("@PostalCode", company.PostalCode);
+            this.db.Execute("usp_UpdateCompany", parameters, commandType: CommandType.StoredProcedure);
+           
             return company;
         }
     }
