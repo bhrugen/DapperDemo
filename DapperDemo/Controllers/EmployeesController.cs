6349e72d20d361d0938b834885fb4f1368b691f5
Ben Spark <bhrugen1990@gmail.com>
2020-07-10 17:52:19 -0500

    Section 4 - Employee Controller

---
+++ DapperDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DapperDemo.Data;
+using DapperDemo.Models;
+using DapperDemo.Repository;
+
+namespace DapperDemo.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ICompanyRepository _compRepo;
+        private readonly IEmployeeRepository _empRepo;
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        public EmployeesController(ICompanyRepository compRepo, IEmployeeRepository empRepo)
+        {
+            _compRepo = compRepo;
+            _empRepo = empRepo;
+        }
+
+        // GET: Companies
+        public async Task<IActionResult> Index()
+        {
+            return View(_empRepo.GetAll());
+        }
+
+      
+
+        // GET: Companies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Companies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public async Task<IActionResult> CreatePOST()
+        {
+            if (ModelState.IsValid)
+            {
+                _empRepo.Add(Employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Companies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Employee = _empRepo.Find(id.GetValueOrDefault());
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+            return View(Employee);
+        }
+
+        // POST: Companies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id != Employee.EmployeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _empRepo.Update(Employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Companies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            _empRepo.Remove(id.GetValueOrDefault());
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
