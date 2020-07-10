c0046ea7a0ceb6cb87ae68db73199600bc2b6824
Ben Spark <bhrugen1990@gmail.com>
2020-07-10 15:08:05 -0500

    Add project files.

---
+++ DapperDemo/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Models
+{
+    public class Company
+    {
+
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
