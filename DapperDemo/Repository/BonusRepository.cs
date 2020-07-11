88e5547035d1dcd7778f1e83958a3221b3a8a641
Ben Spark <bhrugen1990@gmail.com>
2020-07-11 17:27:36 -0500

    Section 7 - Bulk Insert in Dapper

--- DapperDemo/Repository/BonusRepository.cs
+++ DapperDemo/Repository/BonusRepository.cs
@@ -25,18 +25,23 @@ namespace DapperDemo.Repository
                      + "SELECT CAST(SCOPE_IDENTITY() as int); ";
             var id = db.Query<int>(sql, objComp).Single();
             objComp.CompanyId = id;
-           
-            foreach(var employee in objComp.Employees)
-            {
-                employee.CompanyId = objComp.CompanyId;
-                var sql1 = "INSERT INTO Employees (Name, Title, Email, Phone, CompanyId) VALUES(@Name, @Title, @Email, @Phone, @CompanyId);"
-                       + "SELECT CAST(SCOPE_IDENTITY() as int); ";
-                db.Query<int>(sql1, employee).Single();
-            }
 
+            //foreach(var employee in objComp.Employees)
+            //{
+            //    employee.CompanyId = objComp.CompanyId;
+            //    var sql1 = "INSERT INTO Employees (Name, Title, Email, Phone, CompanyId) VALUES(@Name, @Title, @Email, @Phone, @CompanyId);"
+            //           + "SELECT CAST(SCOPE_IDENTITY() as int); ";
+            //    db.Query<int>(sql1, employee).Single();
+            //}
+
+            objComp.Employees.Select(c => { c.CompanyId = id; return c; }).ToList();
+              var sqlEmp = "INSERT INTO Employees (Name, Title, Email, Phone, CompanyId) VALUES(@Name, @Title, @Email, @Phone, @CompanyId);"
+                       + "SELECT CAST(SCOPE_IDENTITY() as int); ";
+
+            db.Execute(sqlEmp, objComp.Employees);
         }
 
-      
+
 
         public List<Company> GetAllCompanyWithEmployees()
         {
