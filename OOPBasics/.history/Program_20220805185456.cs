cf60956c0e3ada4a845793420c5a14df5dfbff56
Fazrin <mfmfazrin1986@gmail.com>
2022-08-05 18:55:24 +0300

    public class Fruit {     public string Name { get; set; }     public string Color { get; set; } }

---
+++ OOPBasics/.history/Program_20220805185456.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Fruit
+{
+    public string Name { get; set; }
+    public string Color { get; set; }
+}
+
+public class Apple : Fruit
+{
+    public string Taste { get; set; }
+
+    public Apple()
+    {
+        Name = "Apple";
+        Color = "Red";
+        Taste = "Sweet";
+    }
+}
+
+public class Mango : Fruit
+{
+    public string Taste { get; set; }
+}
